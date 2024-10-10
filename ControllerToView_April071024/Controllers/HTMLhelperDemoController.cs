b3d191ab4bfcad0b4f340011b91c557edffbadd7
Acer <Acer@DESKTOP-ABKHEEV>
2024-10-10 12:05:11 +0530

    lecture content of 10/10/24 added

---
+++ ControllerToView_April071024/Controllers/HTMLhelperDemoController.cs
@@ -0,0 +1,63 @@
+﻿using ControllerToView_April071024.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerToView_April071024.Controllers
+{
+    public class HTMLhelperDemoController : Controller
+    {
+        public ActionResult dd1()
+        {
+            student s1 = new student() { roll = 1, name = "rajesh" };
+            return View();
+        }
+        public ActionResult demo()
+        {// ADO.net code
+            SqlConnection con = new SqlConnection("Data Source=Desktop-ABKHEEV;Initial Catalog=tempdb;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+            SqlDataAdapter adp = new SqlDataAdapter("select * from tblStudent", con);
+            SqlCommandBuilder cmdb = new SqlCommandBuilder(adp);
+            DataSet ds = new DataSet();
+            adp.Fill(ds, "tblStudent");
+
+            student s1 = new student();
+
+            s1.roll = (int)ds.Tables["tblStudent"].Rows[0][0];
+            s1.name = (string)ds.Tables["tblStudent"].Rows[0][1];
+            return View(s1);
+
+
+
+            /*student s1 = new student();
+            s1.roll = 1;
+            s1.name = "rajesh";
+            
+            return View(s1);
+        */
+            }
+        // GET: HTMLhelperDemo
+        [HttpGet]
+        public ActionResult EmpReg()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult EmpReg(string empid,string empname)
+        {
+            return View();
+        }
+        public ActionResult StudReg()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult StudReg(student s1,string isNewAddmission)
+        {
+            return View();
+        }
+    }
+}
--- ControllerToView_April071024/Models/student.cs
+++ ControllerToView_April071024/Models/student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,11 @@ namespace ControllerToView_April071024.Models
 {
     public class student
     {
+        [Display(Name ="Roll Number")]
         public int roll { get; set; }
+        [Display(Name = "StudentName")]
         public string name { get; set; }
+        public bool admission { get; set; }
+
     }
 }
